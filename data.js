73163a4b63a4e2547eae3da3e77ac0135c45b300
demirose41 <demirose41@gmail.com>
2024-11-21 02:52:27 -0800

    completed practice

--- data.js
+++ data.js
@@ -1,1 +1,17 @@
-// Your code here
+window.addEventListener("DOMContentLoaded", () => {
+
+    
+    const add_button = document.getElementById("add")
+    const list = document.getElementById("shopping-list")
+
+    add_button.addEventListener("click", event => {
+        const type = document.getElementById("type")
+        const name = document.getElementById("name")
+        const newLi =  document.createElement("li");
+        newLi.setAttribute("data-type", type.value);
+        newLi.innerText = name.value;
+        list.appendChild(newLi);
+        event.preventDefault();
+    })
+
+})
